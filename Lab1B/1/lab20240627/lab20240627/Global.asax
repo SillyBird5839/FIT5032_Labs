250dc34ec67d6ab6b67244eb1d502a5f002c476b
SillyBird <1394687881@qq.com>
2024-07-18 00:26:24 +0800

    “Lab_1A_1B_2”

---
+++ Lab1B/1/lab20240627/lab20240627/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab20240627.MvcApplication" Language="C#" %>
---
+++ Lab2/Lab_Snippet/Lab_Snippet/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Lab_Snippet.Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_Snippet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /*
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        */
+
+        public ActionResult About()
+        {
+            // I commented this line out
+            //ViewBag.Message = "Your application description page.";
+            // I created an instance of the Hello object
+            HelloWorld.Hello hello = new HelloWorld.Hello();
+            // I assigned the ViewBag.Message to the result
+            ViewBag.Message = hello.GetHello();
+
+            ExampleDictionary ed = new ExampleDictionary();
+
+            ed.Example();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
